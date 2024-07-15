1bffedd1dbcc23b7f23147e324d752c5965d3115
fishoe <m123oo@naver.com>
2024-07-15 21:38:20 +0900

    create databast dcl in postgresql script

--- db_script_psycopg.py
+++ db_script_psycopg.py
@@ -1,9 +1,16 @@
 import psycopg
+from psycopg import sql
 
-password = "your password here" # 비밀번호를 넣어주세요.
+password = "write your password here!!!" # 비밀번호를 넣어주세요.
 
-# Connect to the PostgreSQL database
-conn = psycopg.connect(dbname="chinook", user="postgres")
+# Connect to the PostgreSQL and create a database
+conn = psycopg.connect(dbname="postgres", user="postgres", password=password)
+conn.autocommit = True
+conn.execute(sql.SQL("CREATE DATABASE {}").format(sql.Identifier("chinook")))
+conn.commit()
+conn.close()
+
+conn = psycopg.connect(dbname="postgres", user="postgres")
 
 # Create a cursor object
 cur = conn.cursor()
